d568838dac6f570a59d9d78c070fda0a9c5636ac
Adzeoda <80847832+Adzeoda@users.noreply.github.com>
2022-12-30 16:40:33 +0000

    dictionary updated

--- anagram_solver.py
+++ anagram_solver.py
@@ -1,26 +1,52 @@
 import json
 import random
 
-filename='dictionary_data.json'
-file= open(filename)
-data=json.load(file)
+filename = 'dictionary_compact.json'
+file = open(filename)
+data = json.load(file)
+
 
 def word_prompt(data, lenght):
-    all_words= list(data.key())
+    all_words = list(data.key())
     while True:
-        word=random.choice(all_words)
-        if len(word)< lenght and len (word)>2:
+        word = random.choice(all_words)
+        if len(word) < lenght and len(word) > 2:
             return word
+
 
 def shuffle_word(word):
     array = list(word)
     shuffled = word
     while True:
         random.shuffle(array)
-        shuffled=''.join(array)
-        if shuffled !=word:
+        shuffled = ''.join(array)
+        if shuffled != word:
             return shuffled
+
 
 print("welcome to the anagram game!")
 while (True):
-    ana
+    word=word_prompt(data,5)
+    question=shuffle_word(word)
+    meaning=data[word]
+
+    question = question.lower()
+    worrd = word.lower()
+
+    print("\nSlove:", question)
+    print("Hint:", meaning)
+
+    for i in range (5,0,-1):
+        print("\nAttempts Left:", i)
+        guess=input('Make a guess: ').lower()
+        if guess==word:
+            print("Correct!")
+            break
+        if i==1:
+            print("\nCorrect answer")
+
+    choice = input("\nContine?[y/n]: ")
+    print('-'*50)
+    if choice == 'n':
+        print("\nThank you for playing")
+        break
